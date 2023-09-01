3156542db160c77ff6e3d38ec84327ee516a47bb
noura965 <noura964@gmail.com>
2023-09-01 15:39:13 +0300

    create the message randomly selecting, using switch case

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ const collectiveWisdom = {
     quote: ['Well done is better than well said', 'Do one thing everyday that scares you', 'Spread love everywhere you go', 'Success is a journey not a destination']
   }
 
+ // Store the 'wisdom' in an array
+  let personalWisdom = []
+  
+  // Iterate over the object
+  for(let prop in collectiveWisdom) {
+    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
+  
+    // use the object's properties to customize the message being added to personalWisdom  
+    switch(prop) {
+      case 'YouAre':
+        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      case 'FocusOn':
+        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      case 'quote':
+        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      default:
+        personalWisdom.push('There is not enough info.')
+    }
+  }
