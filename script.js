696fdc5eaf0e972d000d1058de5dcc4adf117e88
noura965 <noura964@gmail.com>
2023-09-01 15:35:11 +0300

    Create object of arrays

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
   }
 
+const collectiveWisdom = {
+    YouAre: ['Beautiful', 'Cheerful', 'Bright', 'Joyful'],
+    FocusOn: ['Healthy Lifestyle', 'Good time', 'Valuable friends', 'Loved yourself more'],
+    quote: ['Well done is better than well said', 'Do one thing everyday that scares you', 'Spread love everywhere you go', 'Success is a journey not a destination']
+  }
+
