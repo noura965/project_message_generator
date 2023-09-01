8370fa41f41df424e8fa3a8db8fc4650a6657026
noura965 <noura964@gmail.com>
2023-09-01 15:24:37 +0300

    generate random numbers

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ print("Message Generator");
 print("GENERATE");
 print("READ");
 
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+
